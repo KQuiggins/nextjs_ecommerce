fce20f23d6260e3e41a4ec4abb04b0d53a8c86de
KQuiggins <kashfamilyvalues@gmail.com>
2025-02-03 15:22:54 -0500

    feat: refactor header component to use a new menu component and implement responsive design with a sheet for mobile navigation

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,8 @@
-import { ShoppingCart, UserIcon } from "lucide-react"
 import Image from "next/image"
-import { Button } from "@/components/ui/button"
 import { APP_NAME } from "@/lib/constants"
 import Link from "next/link"
-import ModeToggle from "./mode-toggle"
+import Menu from "./menu"
+
 
 const Header = () => {
   return (
@@ -21,19 +20,7 @@ const Header = () => {
             <span className="hidden lg:block text-2xl font-bold ml-3">{APP_NAME}</span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   )
