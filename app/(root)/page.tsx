3b6fd1be6a5131fb68e64eadbe4d01ef239a34e4
KQuiggins <kashfamilyvalues@gmail.com>
2025-02-07 10:10:12 -0500

    feat: add ComicList component and integrate it into the HomePage with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
-
+import sampleData from "@/db/sample-data"
+import ComicList  from "@/components/shared/comics/comic-list"
 
 const HomePage = async () => {
+  
 
   return (
-    <>Comic Store</>
+    <>
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold">Home Page</h1>
+        <ComicList data={sampleData} title="Featured Comics" />
+      </div>
+    </>
   )
 }
 
---
+++ components/shared/comics/comic-list.tsx
@@ -0,0 +1,12 @@
+
+
+const ComicList = ({ data, title }): { data: any; title?: string } => {
+  return (
+    <div className="my-10">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      
+    </div>
+  )
+}
+
+export default ComicList
