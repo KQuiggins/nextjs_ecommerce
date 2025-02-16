b89bdd86a5937be20ff66c022d0085a40c7dd4a1
KQuiggins <kashfamilyvalues@gmail.com>
2025-02-16 15:17:20 -0500

    feat: integrate Neon database adapter and update PrismaClient configuration

--- lib/actions/comic.actions.ts
+++ lib/actions/comic.actions.ts
@@ -1,12 +1,12 @@
 'use server';
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { toJavaScriptObject } from "@/lib/utils";
 import { LATEST_COMICS_LIMIT } from "../constants";
 
 // Get latest comics
 export async function getLatestComics() {
-  const prisma = new PrismaClient();
+  
   const latestComics = await prisma.comic.findMany({
     orderBy: {
       createdAt: 'desc',
