255fd33ae5ec433d86296c696f82ea34223eb148
KQuiggins <kashfamilyvalues@gmail.com>
2025-04-01 06:43:42 -0400

    feat: enhance Place Order functionality with order summary, validation, and improved error handling

--- app/(root)/place-order/place-order-form.tsx
+++ app/(root)/place-order/place-order-form.tsx
@@ -1,9 +1,43 @@
+'use client'
 
+import { useRouter } from 'next/navigation';
+import { Check, Loader } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useFormStatus } from 'react-dom';
+import { createOrderAction } from '@/lib/actions/order.actions';
 
 const PlaceOrderForm = () => {
-  return (
-    <div>PlaceOrderForm</div>
-  )
-}
+  const router = useRouter();
 
-export default PlaceOrderForm
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const res = await createOrderAction();
+
+    if (res.redirectTo) {
+      router.push(res.redirectTo);
+    }
+  };
+
+  const PlaceOrderButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className='w-full'>
+        {pending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Check className='w-4 h-4' />
+        )}{' '}
+        Place Order
+      </Button>
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='w-full'>
+      <PlaceOrderButton />
+    </form>
+  );
+};
+
+export default PlaceOrderForm;
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,9 +1,13 @@
-'use erver'
+'use server'
 
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 import { formatErrors } from "../utils";
 import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
+import { getUserById } from "./user.actions";
+import { insertOrderSchema } from "../validators";
+import { prisma } from "@/db/prisma";
+import { CartItem } from "@/types";
 
 export async function createOrderAction() {
     try {
@@ -14,16 +18,68 @@ export async function createOrderAction() {
         const userId = session?.user?.id;
 
         if (!userId) throw new Error("User not found");
-        
-        const user = await getUserById(userId);
-        if (!user) throw new Error("User not found");
-        if (!cart || cart.items.length === 0){
-            return { success: false, message: "Cart is empty" };
 
+        const user = await getUserById(userId);
+
+        if (!cart || cart.items.length === 0) {
+            return { success: false, message: "Cart is empty", redirectTo: "/cart" };
         }
+        if (!user.address) {
+            return { success: false, message: "No shipping adress", redirectTo: "/heroes-address" };
+        }
+        if (!user.paymentMethod) {
+            return { success: false, message: "No payment method", redirectTo: "/payment-method" };
+        }
+
+        const order = insertOrderSchema.parse({
+            userId: user.id,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+            paymentMethod: user.paymentMethod,
+            shippingAddress: user.address,
+        });
+
+        // Create a transaction to create order and order items in database
+        const insertedOrderId = await prisma.$transaction(async (tx) => {
+            // Create order
+            const insertedOrder = await tx.order.create({ data: order });
+            // Create order items from the cart items
+            for (const item of cart.items as CartItem[]) {
+                await tx.orderItem.create({
+                    data: {
+                        ...item,
+                        price: item.price,
+                        orderId: insertedOrder.id,
+                    },
+                });
+            }
+            // Clear cart
+            await tx.cart.update({
+                where: { id: cart.id },
+                data: {
+                    items: [],
+                    totalPrice: 0,
+                    taxPrice: 0,
+                    shippingPrice: 0,
+                    itemsPrice: 0,
+                },
+            });
+
+            return insertedOrder.id;
+        });
+
+        if (!insertedOrderId) throw new Error('Order not created');
+
+        return {
+            success: true,
+            message: 'Order created',
+            redirectTo: `/order/${insertedOrderId}`,
+        };
     } catch (error) {
         if (isRedirectError(error)) throw error;
         return { success: false, message: formatErrors(error) };
-        
+
     }
 }
